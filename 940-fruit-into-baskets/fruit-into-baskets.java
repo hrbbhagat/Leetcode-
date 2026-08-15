7bb9eeea3031529a0cc46ec1baa609cd97b96341
Harsh kumar <167224476+hrbbhagat@users.noreply.github.com>
2026-08-16 00:31:22 +0530

    Time: 55 ms (42.88%) | Memory: 70.5 MB (56.92%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int k=2;
+         int n = fruits.length;
+
+        int high = 0;
+        int low = 0;
+        int result = -1;
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (high = 0; high < n; high++) {
+
+            // Add current element
+            int num = fruits[high];
+
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
+
+            // If unique elements become more than k
+            while (hm.size() > k) {
+
+                int leftNum = fruits[low];
+
+                hm.put(leftNum, hm.get(leftNum) - 1);
+
+                if (hm.get(leftNum) == 0) {
+                    hm.remove(leftNum);
+                }
+
+                low++;
+            }
+
+            // Exactly k unique elements
+            if (hm.size() == k || hm.size()<k) {
+
+                int length = high - low + 1;
+
+                result = Math.max(result, length);
+            }
+        }
+
+        return result;
+        
+    }
+}
