397f8f4695471f19c4977e5652eff3e8c98bde0b
Harsh kumar <167224476+hrbbhagat@users.noreply.github.com>
2026-08-27 19:43:15 +0530

    Time: 1 ms (97.53%) | Memory: 47.3 MB (60.25%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,12 +1,12 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         
-        int prefix[]=new int[nums.length];
+        int prefix[]=new int[nums.length+1];
         int suffix[]=new int [nums.length];
 
         prefix[0]=0;
       
-        for(int i=1;i<nums.length;i++){
+        for(int i=1;i<=nums.length;i++){
             
             prefix[i]=prefix[i-1]+nums[i-1];
 
@@ -18,7 +18,7 @@ class Solution {
             suffix[j]=suffix[j+1]+nums[j+1];
         }
 
-        
+
         for(int i =0;i<nums.length;i++){
 
         if (prefix[i]==suffix[i]){
