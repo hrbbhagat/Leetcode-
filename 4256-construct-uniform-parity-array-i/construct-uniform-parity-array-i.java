b3b51899ba788706341c4df98708afdf16df14e3
Harsh kumar <167224476+hrbbhagat@users.noreply.github.com>
2026-09-03 00:47:37 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (16.73%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int n = nums1.length;
-        int[] nums2 = new int[n];   // not actually needed
+        // int n = nums1.length;
+        // int[] nums2 = new int[n];   // not actually needed
 
-        int odd = 0;
-        int even = 0;
+        // int odd = 0;
+        // int even = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (nums1[i] % 2 == 0) {
-                even++;
-            } else {
-                odd++;
-            }
-        }
+        // for (int i = 0; i < n; i++) {
+        //     if (nums1[i] % 2 == 0) {
+        //         even++;
+        //     } else {
+        //         odd++;
+        //     }
+        // }
 
-        // Every parity distribution is transformable
+        // // Every parity distribution is transformable
         return true;
       
      
